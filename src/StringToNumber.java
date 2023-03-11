067af2f6f9716a5c0b9e20b580bb81759ad4f275
Robin Schatzl Unix <rsjs1507@gmail.com>
2023-03-11 21:51:50 +0100

    N/A

---
+++ src/StringToNumber.java
@@ -0,0 +1,22 @@
+public class StringToNumber {
+
+    public static void main(String[] args) {
+
+    }
+
+//    }
+//
+//    public static int stringToNumberFail(String str) {
+//        //TODO: Convert str into a number
+//        return int num = Integer.parseInt(str); //hab versucht alles in dem Return zu schreiben
+//    }
+
+    //Endlich die Korrekte lösung
+
+    public static int stringToNumber(String str) {
+        int num = Integer.parseInt(str);        //ist da shier dann die variable mit dem Namen "num" ?
+        return num;
+    }
+}
+
+
