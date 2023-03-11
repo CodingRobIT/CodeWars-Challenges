b8dedfbdb43090fee1f7a8b293563c4832c2b919
Robin Schatzl Unix <rsjs1507@gmail.com>
2023-03-11 22:02:50 +0100

    N/A

---
+++ src/NumberToString.java
@@ -0,0 +1,16 @@
+public class NumberToString {
+
+    public static String numberToString(int num) {
+        // Return a string of the number here!
+        return Integer.toString(num);   //recht schnell via Google gefunden.
+    }
+    public static String numberToString2(int num) {
+        // Return a string of the number here!
+        return num+"";  //Auf CodeWars gefunden.
+    }
+
+    public static String numberToString3(int num) {
+        return String.valueOf(num);   //ebenfalls auf CodeWars gefunden.
+    }
+
+}
