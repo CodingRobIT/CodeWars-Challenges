5d6501ea0049f689647084a51757725f8b566091
Robin Schatzl Unix <rsjs1507@gmail.com>
2023-03-12 13:39:00 +0100

    N/A

--- src/BoolToWord.java
+++ src/BoolToWord.java
@@ -1,7 +1,34 @@
 public class BoolToWord {
 
-    public static String boolToWord(boolean b)
-    {
-        //TODO
+    public static void main(String[] args) {
+        System.out.println(boolToWord(false));
     }
+
+    //    public static String boolToWord(boolean b){       //Scheint nicht zu Funktionieren jan es auf mit einer
+//                                                      //if else funktion versucht
+//        while (b = true) {
+//            return "Yes";
+//        }
+//        while (b = false) {
+//        }
+//        return "No";
+//    }
+//    public static String boolToWord(boolean b) {  //Via google gefunden allerdings wird hier der boolschw wert als text ausgegen
+//        String a = String.valueOf(b);             // Wenn ich es richtig verstanden hab
+//        return a;
+//    }
+
+    public static String boolToWord(boolean b) {
+        if (b == true) {   //vorheriger fehler bei meinem if else war == ich hab lediglich = gemacht
+            return "Yes";
+        } else
+            return "No";
+    }
+
+    public static String boolToWord2(boolean b)  //Andere antworten auf CodeWars
+    {
+        return b ? "Yes" : "No";
+    }
+
+
 }
