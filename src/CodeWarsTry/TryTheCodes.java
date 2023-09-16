46ca4bc0f56a510565f3127c436ee20524c9ec6d
robin <tmai02476@gmail.com>
2023-09-16 18:30:13 +0200

    added Test Class

--- src/CodeWarsTry/TryTheCodes.java
+++ src/CodeWarsTry/TryTheCodes.java
@@ -3,5 +3,15 @@ package CodeWarsTry;
 // This class exists only for testing various code challenges.
 // Because of this, it is mostly half or not complete or without function.
 public class TryTheCodes {
+    public static String smash(String... words) {
 
+        // placeHolder return
+        String placeHolder = "I'm a placeholder text";
+        return placeHolder;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(smash());
+    }
 }
