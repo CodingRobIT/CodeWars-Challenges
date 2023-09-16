c11ffb3952a01379121387f0021ec70f9636c4be
robin <tmai02476@gmail.com>
2023-09-16 20:16:49 +0200

    added Alternativ Codes

--- src/CodeWarsTry/TotalPoints.java
+++ src/CodeWarsTry/TotalPoints.java
@@ -29,3 +29,22 @@ public class TotalPoints {
     }
 
 }
+
+// Found on CodeWars i Like that one
+
+//public class TotalPoints {
+//
+//    public static int points(String[] games) {
+//        int sum = 0;
+//
+//        for (String s : games) {
+//            char x = s.charAt(0),
+//                    y = s.charAt(2);
+//
+//            sum += x > y ? 3 : x == y ? 1 : 0;
+//        }
+//
+//        return sum;
+//    }
+//}
+
