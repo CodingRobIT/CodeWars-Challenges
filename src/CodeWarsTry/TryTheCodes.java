e5fad80f7f2eb29f4c3f4182872c063a8759b89e
robin <tmai02476@gmail.com>
2023-09-16 09:10:58 +0200

    added Test Class

---
+++ src/CodeWarsTry/TryTheCodes.java
@@ -0,0 +1,7 @@
+package CodeWarsTry;
+
+// This class exists only for testing various code challenges.
+// Because of this, it is mostly half or not complete or without function.
+public class TryTheCodes {
+
+}
